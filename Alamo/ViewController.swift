3937ee85cdf7848952655c9917a7d089970fbb52
smeetjordan <smeetb1995@gmail.com>
2017-10-23 11:46:22 +0530

    initial commit

---
+++ Alamo/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Alamo
+//
+//  Created by Smeet Bhatt on 23/10/17.
+//  Copyright © 2017 Smeet Bhatt. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,10 @@
+# Uncomment the next line to define a global platform for your project
+ platform :ios, '9.0'
+
+target 'Alamo' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Alamo
+pod 'Alamofire', '~> 4.5'
+end
